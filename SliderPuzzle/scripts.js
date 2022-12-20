04fa879baa62861af9123fe791b54d70629fcfdc
Neill10 <95107962+Neill10@users.noreply.github.com>
2022-12-20 14:15:08 -0500

    updated from classwork

--- SliderPuzzle/scripts.js
+++ SliderPuzzle/scripts.js
@@ -1,12 +1,32 @@
 function initialize(){
-    diceTable = document.getElementById("dicetable");
-    cellArray = [];
+    //diceTable = document.getElementById("dicetable");
+
+    c1 = document.getElementById("cell1");
+    c2 = document.getElementById("cell2");
+    c3 = document.getElementById("cell3");
+    c4 = document.getElementById("cell4");
+    c5 = document.getElementById("cell5");
+    c6 = document.getElementById("cell6");
+    c7 = document.getElementById("cell7");
+    c8 = document.getElementById("cell8");
+    c9 = document.getElementById("cell9");
+    c10 = document.getElementById("cell10");
+    c11 = document.getElementById("cell11");
+    c12 = document.getElementById("cell12");
+    c13 = document.getElementById("cell13");
+    c14 = document.getElementById("cell14");
+    c15 = document.getElementById("cell15");
+    c16 = document.getElementById("cell16");
+
+    cellArray = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16];
     createTable();
     console.log(cellArray);
+    selected = "";
 }
 
 function createTable(){
     var num = 0;
+    /*
     for (var i = 0; i < 4; i++){
         var newRow = diceTable.insertRow();
         
@@ -16,6 +36,7 @@ function createTable(){
             if(num != 15)
             {
                 newCell.innerHTML = num + 1;
+                newCell.innerHTML.onclick = "BRUH";
                 num++;
             }
             else{
@@ -24,6 +45,7 @@ function createTable(){
             cellArray.push(newCell);
         }
     }
+    */
 }
 
 function select(){
