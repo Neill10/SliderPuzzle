796d8fcf6f1995e0be12dc19754663ce064d8f5b
Neil Liu <Neill10@nycstudents.net>
2022-12-22 14:01:05 -0500

    started calculating valid move logic

--- scripts.js
+++ scripts.js
@@ -20,18 +20,21 @@ function initialize(){
     c16 = document.getElementById("cell16");
 
     cellArray = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16];
-    createTable();
+    //createTable();
     console.log(cellArray);
     //the cell that is selected
     selected = "";
     //counter of selected cells 
     selectCounter = 0;
+    //blank cell
+    blankCell = c16;
 
-
-    for(cell of cellArray)
+    for(var i = 0; i < cellArray.length ; i++)
     {
-        cell.selected = false;
+        cellArray[i].selected = false;
         console.log(cell.selected);
+        //cell position [1-16] (used to calculate valid moves up down left right)
+        cellArray[i].cellPosition = i + 1;
     }
 }
 
@@ -58,7 +61,50 @@ function createTable(){
     }
     */
 }
-
-function select(cell){
+//checks if the two selected cells are valid to move(one of them is a blank)
+function validMove(secondCell){
     
+}
+
+//@returns an array of valid moves
+//calculates the valid move
+function calculateValid(cell){
+    var valid = [];
+    var position = cell.getPosition;
+    var up = -1;
+    var down = -1;
+    var left = -1;
+    var right = -1;
+    if (!position <= 4){
+        up = position - 4;
+        valid.push(up);
+    }
+    if(position >= 16){ 
+        down = position + 4;
+        valid.push(down);
+    }
+    if(!(position == 1 || position == 5 || position == 9 || position == 13)){ 
+        left = position - 1;
+        valid.push(left);
+    }
+    if(!(position == 4 || position == 8 || position == 12 || position == 16)){ 
+        right = position + 1;
+        valid.push(right);
+    }
+    //calculates for edge of the board and corners.
+    
+
+}
+
+
+//redo function.. create validMove first
+function select(cell){
+    selectCounter++;
+    if(selectCounter == 1)
+    {
+        selected = cell;
+    }
+    else if(selectCounter == 2){
+        validMove(cell);
+    }
 }
