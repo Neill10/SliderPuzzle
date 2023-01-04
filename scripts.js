8dd68d867bd6cca6d5f4d700518aab5f8a4771ef
Neill10 <95107962+Neill10@users.noreply.github.com>
2023-01-03 19:31:24 -0500

    finished lvl3

--- scripts.js
+++ scripts.js
@@ -18,6 +18,8 @@ function initialize(){
     c14 = document.getElementById("cell14");
     c15 = document.getElementById("cell15");
     c16 = document.getElementById("cell16");
+    winDisplay = document.getElementById("winDisplay");
+    counterDisplay = document.getElementById("counterDisplay");
 
     cellArray = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16];
     //createTable();
@@ -26,7 +28,9 @@ function initialize(){
     selected = "";
     //counter of selected cells 
     selectCounter = 0;
-    //blank cell
+    //counts the player moves
+    counter = 0;
+    //blank cell (currently not in use)
     blankCell = c16;
 
     for(var i = 0; i < cellArray.length ; i++)
@@ -36,8 +40,9 @@ function initialize(){
         cellArray[i].cellPosition = i + 1;
         //console.log(cellArray[i].cellPosition);
         //the current number that is displayed at this cell
+        
         cellArray[i].display = i + 1;
-
+        console.log(cellArray[i].display);
     }
 }
 
@@ -64,7 +69,11 @@ function createTable(){
     }
     */
 }
-//redo function.. create validMove first
+
+//This function will do two different tasks based on the number of times it is run
+// 1st: stores the first cell that the user clicks (selected)
+// 2nd: stores the second cell (cell) and determines whether the two selected cells is c16
+// if it is c16 (blank) it will then determine valid moves and then swap.
 function select(cell){
     selectCounter++;
     if(selectCounter == 1)
@@ -72,11 +81,28 @@ function select(cell){
         selected = cell;
     }
     else if(selectCounter == 2){
-        if (validMove(cell)){
-            slide(selected,cell);
+        //will only run when one of the selected cells is the c16 or "BLANK" cell
+        if(selected.display == 16 || cell.display == 16){
+            if (validMove(cell)){
+                slide(selected,cell);
+                //changes the innerHTML of c16 to BLANK (for the user)
+                if(selected.display == 16)
+                {
+                    selected.innerHTML = "";
+                }
+                else{
+                    cell.innerHTML = "";
+                }
+                counter++;
+                counterDisplay.innerHTML = counter;
+                win();
+            }
+            else{
+                console.log("not a valid move");
+            }
         }
         else{
-            console.log("not a valid move");
+            console.log("BLANK is not selected");
         }
         selectCounter = 0;
     }
@@ -95,7 +121,6 @@ function validMove(secondCell){
     return false;
 }
 
-//need to fix this function
 //@returns an array of valid moves
 //calculates the valid move
 function calculateValid(cell){
@@ -107,12 +132,12 @@ function calculateValid(cell){
     var right = -1;
     //calculates for edge of the board and corners.
     //up valid moves
-    if (!position1 <= 4){
+    if (!(position1 <= 4)){
         up = position1 - 4;
         valid.push(up);
     }
     //down valid moves
-    if(!position1 >= 13){ 
+    if(!(position1 >= 13)){ 
         down = position1 + 4;
         valid.push(down);
         console.log(down);
@@ -131,19 +156,59 @@ function calculateValid(cell){
     return valid;
 }
 
-//swaps the innnerHTML of the cell
+//swaps the innerHTML of the cell
 function slide(first, second){
     
     var tempDisplay = second.display;
     second.display = first.display;
     first.display = tempDisplay;
 
-    console.log(tempDisplay);
-    console.log(first.display);
-    console.log(second.display);
+    //console.log("TEMP " + tempDisplay);
+    //console.log("FIRST " + first.display);
+    //console.log("SECOND " + second.display);
 
+    console.log("FIRST pos " + first.cellPosition);
+    console.log("SECOND pos " + second.cellPosition);
     second.innerHTML = second.display;
-    first.innnerHTML = first.display;
+    first.innerHTML = first.display;
 }
 
+//slides the board 1000 times
+function shuffle1000(){
+    for(var i = 0; i < 1000; i++){
+        console.log("I: " + i);
+        //searches through cellArray for display of 16
+        for(tempCell of cellArray){
+            if(tempCell.display == 16){
+                blankCell = tempCell;
+            }
+        }
+        validMoveArray = calculateValid(blankCell);
+        var random = Math.floor(Math.random() * validMoveArray.length);
+        //console.log("blank" + blankCell.display);
+        //console.log("second" + cellArray[validMoveArray[random] - 1]);
 
+        slide(blankCell, cellArray[validMoveArray[random] - 1]);
+        
+        if(blankCell.display == 16)
+        {
+            blankCell.innerHTML = "";
+        }
+        else{
+            cellArray[validMoveArray[random] - 1].innerHTML = "";
+        }
+    }
+}
+
+function win(){
+    var win = true;
+    for(var i = 0; i < cellArray.length;i++){
+        if(cellArray[i].display != i + 1){
+            win = false;
+            winDisplay.innerHTML = ""
+        }
+    }
+    if(win){
+        winDisplay.innerHTML = "YOU WON! Congrats";
+    }
+}
